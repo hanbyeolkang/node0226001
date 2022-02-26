86db3194a11aeb14d87e6a6757f01ba453eb4b6c
hanbyeolkang <strongstar1210@gmail.com>
2022-02-26 23:15:31 +0900

    로그인 기능 구현

--- app/app.js
+++ app/app.js
@@ -2,6 +2,7 @@
 
 // module
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 
 // routing
@@ -10,8 +11,10 @@ const home = require("./src/routes/home");
 // app setting
 app.set("views", "./src/views");
 app.set("view engine", "ejs");
-
 app.use(express.static(`${__dirname}/src/public`));
+app.use(bodyParser.json());
+// url을 통해 전달되는 데이터에 한글, 공백 등과 같은 문자가 포함될 경우 제대로 인식되지 않는 문제 해결
+app.use(bodyParser.urlencoded({ extended:true }));
 app.use("/", home); // use -> 미들웨어 등록 메서드
 
 module.exports = app;
--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,14 +1,41 @@
 "use strict";
 
-const home = (req, res) => {
-    res.render("home/index");
+const output = {
+    home: (req, res) => {
+        res.render("home/index");
+    },
+    login: (req, res) => {
+        res.render("home/login");
+    },
+}
+
+const users = {
+    id: ["id1", "id2", "id3"],
+    psword: ["pw1", "pw2", "pw3"],
 };
 
-const login = (req, res) => {
-    res.render("home/login");
-};
+const process = {
+    login: (req, res) => {
+        const id = req.body.id,
+            psword = req.body.psword;
+        
+        if (users.id.includes(id)) {
+            const idx = users.id.indexOf(id);
+            if (users.psword[idx] === psword) {
+                return res.json({
+                    success: true,
+                });
+            }
+        }
+
+        return res.json({
+            success: false,
+            msg: "로그인 실패",
+        });
+    }
+}
 
 module.exports = {
-    home,
-    login,
+    output,
+    process
 };
