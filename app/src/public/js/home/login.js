21965ee5aa8794f3c8a07275416bd7a102a10d31
hanbyeolkang <strongstar1210@gmail.com>
2022-02-27 13:50:56 +0900

    회원가입 라우팅 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -2,7 +2,7 @@
 
 const id = document.querySelector("#id"),
     password = document.querySelector("#psword"),
-    loginBtn = document.querySelector("button");
+    loginBtn = document.querySelector("#button");
 
 loginBtn.addEventListener("click", login);
 
